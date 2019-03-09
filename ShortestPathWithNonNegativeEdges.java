b244d226e21fb95de20d17afe140303fd13da0e9
Mikhail Palagashvili <48160170+MikhailPalagashvili@users.noreply.github.com>
2019-03-09 16:10:15 -0500

    Update ShortestPathWithNonNegativeEdges.java

--- ShortestPathWithNonNegativeEdges.java
+++ ShortestPathWithNonNegativeEdges.java
@@ -1,4 +1,3 @@
-import java.util.PriorityQueue;
 import java.util.Stack;
 
 public class ShortestPathWithNonNegativeEdges {
@@ -32,7 +31,7 @@ public class ShortestPathWithNonNegativeEdges {
             distTo[w] = distTo[v] + e.weight();
             edgeTo[w] = e;
             if (pq.contains(w)) pq.decreaseKey(w, distTo[w]);
-            else                pq.insert(w, distTo[w]);
+            else pq.insert(w, distTo[w]);
         }
     }
 
@@ -40,10 +39,40 @@ public class ShortestPathWithNonNegativeEdges {
         return this.distTo[v];
     }
 
-    public Iterable<DirectedEdge> pathTo(int v) {
-        Stack<DirectedEdge> path = new Stack <>();
+    public Iterable <DirectedEdge> pathTo(int v) {
+        Stack <DirectedEdge> path = new Stack <>();
         for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()])
             path.push(e);
         return path;
+    }
+
+    public static void main(String[] args) {
+        final int vertices = 8;
+        final int source = 0;
+        EdgeWeightedDiGraph graph = new EdgeWeightedDiGraph(vertices);
+        System.out.println(graph.getVertices());
+        System.out.println("***********");
+        graph.addEdge(new DirectedEdge(0, 1, 5));
+        graph.addEdge(new DirectedEdge(0, 4, 9));
+        graph.addEdge(new DirectedEdge(0, 7, 8));
+        graph.addEdge(new DirectedEdge(1, 2, 12));
+        graph.addEdge(new DirectedEdge(1, 3, 15));
+        graph.addEdge(new DirectedEdge(1, 7, 4));
+        graph.addEdge(new DirectedEdge(2, 3, 3));
+        graph.addEdge(new DirectedEdge(2, 6, 11));
+        graph.addEdge(new DirectedEdge(3, 6, 9));
+        graph.addEdge(new DirectedEdge(4, 5, 4));
+        graph.addEdge(new DirectedEdge(4, 6, 20));
+        graph.addEdge(new DirectedEdge(4, 7, 5));
+        graph.addEdge(new DirectedEdge(5, 2, 1));
+        graph.addEdge(new DirectedEdge(5, 6, 13));
+        graph.addEdge(new DirectedEdge(7, 5, 6));
+        graph.addEdge(new DirectedEdge(7, 2, 7));
+        ShortestPathWithNonNegativeEdges sp = new ShortestPathWithNonNegativeEdges(graph, source);
+        for (int v = 0; v < graph.getVertices(); v++) {
+            System.out.println(sp.distTo(v));
+            System.out.println(sp.pathTo(v));
+            System.out.println();
+        }
     }
 }
